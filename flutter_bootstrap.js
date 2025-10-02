a4d7b128ba117a79f6f6055eb88c4ea18aaceec8
Beshoy Akram <beshoyakram22@gmail.com>
2025-10-03 02:33:00 +0300

    Test -1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4204781983"
+    serviceWorkerVersion: "3293752590"
   }
 });
